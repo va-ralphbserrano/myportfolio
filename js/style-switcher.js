8b6b889fde20af24327246bea32157acf9466835
va-ralphbserrano <ralph.b.serrano@gmail.com>
2024-10-30 22:33:43 +0800

    Website updates

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -33,12 +33,7 @@ dayNight.addEventListener("click", () => {
 });
 
 window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark")) {
-        dayNight.querySelector("i").classList.add("fa-sun");
-
-        dayNight.querySelector("i").classList.add("fa-spin"); // Add spin for sun
-    } else {
-        dayNight.querySelector("i").classList.add("fa-moon");
-        dayNight.querySelector("i").classList.add("fa-bounce"); // Add bounce for moon
-    }
+    document.body.classList.add("dark");
+    dayNight.querySelector("i").classList.add("fa-sun");
+    dayNight.querySelector("i").classList.add("fa-spin"); // Add bounce for moon
 });
